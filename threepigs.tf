551d3b323cb1f93ab40b5d9aefcf564ab2c02ded
sum <chempionistr@gmail.com>
2021-02-07 15:38:20 +0200

    changed

--- threepigs.tf
+++ threepigs.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "MyFirstInstance1" {
-  count         = 5
+  count         = 3
   ami           = "ami-096cdd827d38ca4d8"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "motp-${count.index}"
+    Name = "threepigs-${count.index}"
   }
 }
 
