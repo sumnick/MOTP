a760e65911ab40c8e5133191962b14f22c063d9d
sum <chempionistr@gmail.com>
2021-02-07 15:33:05 +0200

    change name

--- threepigs.tf
+++ threepigs.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "MyFirstInstance1" {
-  count         = 3
+  count         = 5
   ami           = "ami-096cdd827d38ca4d8"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "threepigs-${count.index}"
+    Name = "motp-${count.index}"
   }
 }
 
